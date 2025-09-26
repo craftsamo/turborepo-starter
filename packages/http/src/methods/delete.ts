2c67eaaa58d2f6114f0b7baed81f0f16bf977b0d
CraftSamo <craftsamo.g@gmail.com>
2025-09-26 11:45:06 +0900

    refactor(web): Refactor web to use shared HTTP package, remove legacy modules (#48)
    ---
    CraftSamo -> feat(http): add FetcherError for HTTP error handling
    CraftSamo -> feat(http): add ApiError for REST API error handling
    CraftSamo -> feat(http): add http function for typed fetch requests
    CraftSamo -> feat(http): add utility functions for fetch requests
    CraftSamo -> feat(http): add REST method wrappers for fetcher
    CraftSamo -> chore(http): add config and setup files for package
    CraftSamo -> chore(workflows): add label for http package
    CraftSamo -> refactor(web): remove legacy fetcher and error modules
    CraftSamo -> refactor(web): migrate to shared http package
    CraftSamo -> chore: fix typo and add api dev script
    ---

--- packages/http/src/methods/delete.ts
+++ packages/http/src/methods/delete.ts
@@ -1,17 +1,18 @@
 import { buildCredentials, buildHeaders, buildPathWithSearchParams } from '../utils';
 import { http, type FetchResult } from '../http';
-import { type PostOptions } from './post';
+import { GetOptions } from './get';
 
 /**
  * Sends a DELETE request to the specified path.
  *
  * @template T - The expected response type.
  * @param {string} path - The API endpoint path.
- * @param {Options<T>} [options] - The request options.
+ * @param {GetOptions<U>} [options] - The request options.
  */
-export async function remove<T = object>(path: string, options?: PostOptions<T>): Promise<FetchResult<T>> {
+export async function remove<T = object, U = object>(path: string, options?: GetOptions<U>): Promise<FetchResult<T>> {
   return http<T>(buildPathWithSearchParams(path, options?.params ? options.params : undefined), {
     method: 'DELETE',
+    ...options,
     headers: buildHeaders(options?.headers),
     credentials: buildCredentials(options?.credentials),
   });
