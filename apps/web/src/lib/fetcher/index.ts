a686ed7088b0826c7bede49354aa73dbace85a3a
CraftSamo <craftsamo.g@gmail.com>
2025-09-26 11:45:06 +0900

    refactor(web): Refactor web to use shared HTTP package, remove legacy modules (#48)
    ---
    CraftSamo -> feat(http): add FetcherError for HTTP error handling
    CraftSamo -> feat(http): add ApiError for REST API error handling
    CraftSamo -> feat(http): add http function for typed fetch requests
    CraftSamo -> feat(http): add utility functions for fetch requests
    CraftSamo -> feat(http): add REST method wrappers for fetcher
    CraftSamo -> chore(http): add config and setup files for package
    CraftSamo -> chore(workflows): add label for http package
    CraftSamo -> refactor(web): remove legacy fetcher and error modules
    CraftSamo -> refactor(web): migrate to shared http package
    CraftSamo -> chore: fix typo and add api dev script
    ---

--- apps/web/src/lib/fetcher/index.ts
+++
@@ -1,4 +0,0 @@
-export * as fetcher from './methods';
-export * from './utils';
-export * from './errors';
-export * from './http';
--- apps/web/src/lib/fetcher/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './buildCredentials';
-export * from './buildFullPath';
-export * from './buildHeaders';
-export * from './buildPathWithSearchParam';
-export * from './buildRequestBody';
